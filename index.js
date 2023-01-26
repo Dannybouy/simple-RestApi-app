ad8a6a88adf47e215907d55e06ada36ca9b2dd6b
Dannybouy <danielokpara29@gmail.com>
2023-01-26 20:15:11 +0100

    added a then and catch func to connect func

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser'; // allows you to take in incoming post requests bodys
 import mongoose from 'mongoose';
-import env from 'dotenv/config';
+import dotenv from 'dotenv';
 
 import usersRoutes from './routes/users.js'
 import cors from 'cors';
 
 const app = express(); // calling express
 const PORT = 5000; // for our port
-
+dotenv.config()
 app.use(cors());
 app.use(bodyParser.json()); // body parser middleware, it just tells us that we would be using json in our app
 
@@ -22,11 +22,10 @@ app.get('/', (req, res) => res.send('Hello World! from Homepage')); // get reque
 
 //Connecting to the database
 mongoose.set("strictQuery", false)
-mongoose.connect(process.env.DB_CONNECTION,
-    { useNewUrlParser:true, useUnifiedTopology: true }, 
-    () => console.log('Connected to database!')
-    
-)
+
+mongoose.connect(process.env.DB_CONNECTION)
+.then(() => console.log("Connected to database!"))
+.catch(err => console.log(err))
     
 // Creating a listening port
-app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`))
